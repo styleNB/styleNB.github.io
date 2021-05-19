cba82ac60f8106e0346dad11f0e1bec1bf027864
styleNB <yujiaming139147@vip.qq.com>
2021-05-20 03:06:15 +0800

    111111

--- js/chunk-2d230fe7.4c2904b0.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230fe7"],{ef3c:function(e,n,r){"use strict";r.r(n);r("0482"),r("a5bc");var t,u,a={created:function(){console.log(1111111111);var e=this.$route,n=e.params,r=e.query,t=n.path;this.$router.replace({path:"/"+t,query:r})},render:function(e){return e()}},c=a,o=r("5d22"),l=Object(o["a"])(c,t,u,!1,null,null,null);n["default"]=l.exports}}]);
-//# sourceMappingURL=chunk-2d230fe7.4c2904b0.js.map
